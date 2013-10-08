0904b40b78ab32aeb250d2093f44b635785da675
Adamczak <krystian@debian>
2013-10-08 21:17:02 +0200

    poprawiona funkcja sortowania w insertion

--- lista1/src/InsertionSort.java
+++ lista1/src/InsertionSort.java
@@ -5,33 +5,43 @@ import java.util.ArrayList;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author krystian
  */
-public class InsertionSort extends SortBase{
+public class InsertionSort extends SortBase {
 
     public InsertionSort(int n) {
         super(n);
     }
-    
+
     @Override
     public ArrayList<Integer> sort(ArrayList<Integer> arr) {
         int tmp;
         ArrayList<Integer> temparr = new ArrayList<>(arr);
-        
-        for(int i=0 ; i<temparr.size() ; i++) {
-            for(int j=0 ; j<temparr.size() ; j++) {
-                if(temparr.get(i) > temparr.get(j)){}
-                else {
-                    tmp = temparr.get(i);
-                    temparr.set(i, temparr.get(j));
-                    temparr.set(j, tmp);
-                }
+
+        for (int i = 1; i < temparr.size(); i++) {
+            int j = i;
+            tmp = temparr.get(i);
+            while (j > 0 && tmp < temparr.get(j - 1)) {
+                temparr.set(j, temparr.get(j - 1));
+                j--;
             }
+            temparr.set(j, tmp);
         }
         return temparr;
     }
-    
+    /*
+     * for (int i = 1; i < n; i++){
+     int j = i;
+     int B = array[i];
+     while ((j > 0) && (array[j-1] > B)){
+     array[j] = array[j-1];
+     j--;
+     }
+     array[j] = B;
+     }
+     }
+     }
+     */
 }
